4aab8fb9e9d73665934e4da469b70e22a9bd8e43
oferovadia <ovadiaofer@gmail.com>
2022-08-18 17:56:34 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import AllProducts from './components/Carousel/AllProducts';
 import './App.css';
 import Login from './components/Login/Login';
+import About from './components/About/About';
 import NavbarComp from './components/Navbar/Navbar';
 import ProductPage from './components/ProductPage/ProductPage';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <NavbarComp></NavbarComp>
-      <ProductPage></ProductPage>
+      <Routes>
+        <Route path='/login' element={<Login></Login>} />
+        <Route path='/product' element={<ProductPage></ProductPage>} />
+        <Route path='/products' element={<AllProducts></AllProducts>} />
+        <Route path='/about' element={<About></About>} />
+
+      </Routes>
     </div>
   );
 }
